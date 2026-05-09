c4753256b73ec8190221115cb871f706a03b1e18
Mohamed I. <281434933+ir7abi@users.noreply.github.com>
2026-05-09 17:22:08 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-/* Mohamed Irhabi — interactions */
-(function () {
-  'use strict';
-
-  // Footer year
-  const yearEls = document.querySelectorAll('#year');
-  yearEls.forEach(el => { el.textContent = new Date().getFullYear(); });
-
-})();
